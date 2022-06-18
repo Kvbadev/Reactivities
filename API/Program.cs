dbeb760d9e25236980d80c562d48d43051b20e22
Jakub Piasecki <jakubpiasecki05@gmail.com>
2022-06-18 18:00:08 +0200

    Completed walking skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//
+
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy => {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -16,6 +24,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 var app = builder.Build();
 
+//seed the data in the database if it does not exist yet
 using (var scope = app.Services.CreateScope()) //Dispose() method ends the scope lifetime, that method is invoked when using scope ends
 {
     try{
@@ -40,6 +49,8 @@ if (app.Environment.IsDevelopment())
 // app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
